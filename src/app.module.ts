52793d7c4b93ed4aa6d0bc2336d1afc0b9dbe9df
Vimal Raj <vimal@vajro.com>
2021-12-12 00:46:54 +0530

    project setup done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
+import {GraphQLModule} from '@nestjs/graphql'
+import {ApolloServerPluginLandingPageLocalDefault} from 'apollo-server-core'
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { join } from 'path';
+import {PrismaService} from './prisma.service'
 
 @Module({
-  imports: [],
+  imports: [GraphQLModule.forRoot({
+    plugins:[ApolloServerPluginLandingPageLocalDefault()],
+    autoSchemaFile:join(process.cwd(),'schema.gql'),
+    introspection:true,
+    playground:false
+  })],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,PrismaService],
 })
 export class AppModule {}
